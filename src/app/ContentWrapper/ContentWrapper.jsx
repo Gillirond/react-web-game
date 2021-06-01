7945bdabb3e21391e6c797662d1dc8807ccd7556
Gillirond <bogyn@voliacable.com>
2021-06-01 03:21:11 +0300

    initial

---
+++ src/app/ContentWrapper/ContentWrapper.jsx
@@ -0,0 +1,11 @@
+import React, {Component} from "react";
+
+export default class ContentWrapper extends Component {
+    render() {
+        return (
+            <div>
+                CONTENT
+            </div>
+        )
+    }
+}
---
+++ src/app/app.jsx
@@ -0,0 +1,9 @@
+import ReactDOM from 'react-dom';
+import React from 'react';
+import ContentWrapper from './ContentWrapper/ContentWrapper.jsx';
+
+
+ReactDOM.render(
+    <ContentWrapper />,
+    document.getElementById("root")
+)
